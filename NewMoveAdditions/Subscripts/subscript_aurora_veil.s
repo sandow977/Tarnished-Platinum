6a08249be2d78a3682786a51cf4d07ee5dde5eb5
Sandow <ageofsandow15@gmail.com>
2026-03-08 16:25:20 -0500

    Add files via upload

---
+++ NewMoveAdditions/Subscripts/subscript_aurora_veil.s
@@ -0,0 +1,12 @@
+#include "macros/btlcmd.inc"
+
+_000:
+    TryReflect _010
+    Call BATTLE_SUBSCRIPT_ANIMATION_PREPARED_MESSAGE
+
+_010:
+    TryLightScreen _020
+    Call BATTLE_SUBSCRIPT_ANIMATION_PREPARED_MESSAGE
+
+_020:
+    End
