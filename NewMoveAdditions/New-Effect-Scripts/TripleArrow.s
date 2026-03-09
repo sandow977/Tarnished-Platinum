6caf7653c729140aafd69095fadcbb2ba12e231f
Sandow <ageofsandow15@gmail.com>
2026-03-09 12:21:23 -0500

    Add files via upload

---
+++ NewMoveAdditions/New-Effect-Scripts/TripleArrow.s
@@ -0,0 +1,9 @@
+#include "macros/btlcmd.inc"
+
+
+_000:
+    UpdateVar OPCODE_ADD, BTLVAR_CRITICAL_BOOSTS, 1
+    UpdateVar OPCODE_SET, BTLVAR_SIDE_EFFECT_FLAGS_INDIRECT, MOVE_SIDE_EFFECT_TO_DEFENDER|MOVE_SUBSCRIPT_PTR_TRIPLE_ARROW
+    CalcCrit 
+    CalcDamage 
+    End 
